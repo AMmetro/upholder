37414a497e8dbad22ae52c4d7188b263d5d14f2d
AMmetro <metro.trend.now@gmail.com>
2022-12-28 22:15:36 +0300

    add 1-1 number comment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2
+          Learn React 1-1
         </a>
       </header>
     </div>
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+//1-1
